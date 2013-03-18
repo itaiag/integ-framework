dfe857996d3f62590739888c0fbce50af36dd8c8
itaiag <itai.agmon@gmail.com>
2013-03-18 14:00:07 +0200

    Database system module and numbercompare assertion

--- integ-db/src/main/java/il/co/topq/integframework/db/DatabaseSystemModule.java
+++ integ-db/src/main/java/il/co/topq/integframework/db/DatabaseSystemModule.java
@@ -1,14 +1,19 @@
 package il.co.topq.integframework.db;
 
+import il.co.topq.integframework.assertion.Assert;
+import il.co.topq.integframework.assertion.NumberCompareAssertion;
+import il.co.topq.integframework.assertion.NumberCompareAssertion.CompareMethod;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 
-public class DatabaseSystemModule  {
+public class DatabaseSystemModule {
 
 	private final JdbcTemplate template;
 	private List<ResultSetPrinter> resultSetPrinterList;
@@ -16,11 +21,36 @@ public class DatabaseSystemModule  {
 	public DatabaseSystemModule(final DataSource dataSource) {
 		super();
 		this.template = new JdbcTemplate(dataSource);
-		resultSetPrinterList = new ArrayList<ResultSetPrinter>();
-		resultSetPrinterList.add(new ResultSetDefaultPrinter());
 	}
 
-	private List<Map<String, Object>> getResultList(String sql) {
+	@PostConstruct
+	public void initResultSetPrinters() {
+		resultSetPrinterList = new ArrayList<ResultSetPrinter>();
+		resultSetPrinterList.addAll(tablePrinters());
+	}
+
+	/**
+	 * Override this method if you want to add more table printers. For example
+	 * if you want to make use of the ResultSetHTMLPrinter
+	 * 
+	 * @return The table printers to add for each query
+	 */
+	protected List<ResultSetPrinter> tablePrinters() {
+		List<ResultSetPrinter> printers = new ArrayList<ResultSetPrinter>();
+		printers.add(new ResultSetHTMLPrinter());
+		return printers;
+	}
+
+	/**
+	 * 
+	 * @param sql
+	 * @return A list that representing the result set of the given query
+	 */
+	public List<Map<String, Object>> getResultList(final String sql) {
+		if (null == sql || sql.isEmpty()) {
+			throw new IllegalArgumentException("SQL query can't be empty");
+		}
+
 		List<Map<String, Object>> resultList = template.queryForList(sql);
 		for (ResultSetPrinter printer : resultSetPrinterList) {
 			printer.print(resultList);
@@ -28,7 +58,36 @@ public class DatabaseSystemModule  {
 		return resultList;
 	}
 
-	public int countRowsInTable(String table) {
+	/**
+	 * Compare between the number of rows returned as a result of executing the
+	 * specified query and the given number.
+	 * 
+	 * @param sql
+	 *            SQL query
+	 * @param expectedNumOfRows
+	 *            the number of rows that are expected to return from the query
+	 * @param compareMethod
+	 *            The relations between the actual number of rows and the
+	 *            expected
+	 * @throws Exception
+	 */
+	public void assertNumOfRows(final String sql, int expectedNumOfRows, CompareMethod compareMethod) throws Exception {
+		int actual = getResultList(sql).size();
+		Assert.assertLogic(actual, new NumberCompareAssertion(expectedNumOfRows, compareMethod));
+	}
+
+	public void assertNumOfRows(final String sql, int expectedNumOfRows) throws Exception {
+		assertNumOfRows(sql, expectedNumOfRows, CompareMethod.EQUALS);
+	}
+
+	/**
+	 * @param table
+	 * @return the number of rows in the specified table
+	 */
+	public int countRowsInTable(final String table) {
+		if (null == table || table.isEmpty()) {
+			throw new IllegalArgumentException("Table name can't be empty");
+		}
 		return getResultList(String.format("SELECT * FROM %s", table)).size();
 	}
 
---
+++ integ-testng/src/main/java/il/co/topq/integframework/assertion/NumberCompareAssertion.java
@@ -0,0 +1,90 @@
+package il.co.topq.integframework.assertion;
+
+public class NumberCompareAssertion extends AbstractAssertionLogic {
+
+	public enum CompareMethod {
+		BIGGER, BIGGER_OR_EQUALS, EQUALS, SMALLER_OR_EQUALS, SMALLER
+
+	}
+
+	private final int expected;
+
+	private int actualInt;
+
+	private final CompareMethod compareMethod;
+
+	public NumberCompareAssertion(int expected, CompareMethod compareMethod) {
+		super();
+		this.expected = expected;
+		this.compareMethod = compareMethod;
+	}
+
+	@Override
+	public void doAssertion() {
+		status = true;
+		switch (compareMethod) {
+		case BIGGER:
+			if (actualInt > expected) {
+				title = "Actual number " + actualInt + " is bigger then " + expected;
+			} else {
+				title = "Actual number " + actualInt + " is NOT bigger then " + expected;
+				status = false;
+			}
+			break;
+		case BIGGER_OR_EQUALS:
+			if (actualInt >= expected) {
+				title = "Actual number " + actualInt + " is bigger or equals to " + expected;
+			} else {
+				title = "Actual number " + actualInt + " is NOT bigger or equals to " + expected;
+				status = false;
+			}
+
+			break;
+		case EQUALS:
+			if (actualInt == expected) {
+				title = "Actual number " + actualInt + " is equals to " + expected;
+			} else {
+				title = "Actual number " + actualInt + " is NOT equals to " + expected;
+				status = false;
+			}
+			break;
+
+		case SMALLER_OR_EQUALS:
+			if (actualInt <= expected) {
+				title = "Actual number " + actualInt + " is smaller or equals to " + expected;
+			} else {
+				title = "Actual number " + actualInt + " is NOT smaller or equals to " + expected;
+				status = false;
+			}
+			break;
+		case SMALLER:
+			if (actualInt < expected) {
+				title = "Actual number " + actualInt + " is smaller then " + expected;
+			} else {
+				title = "Actual number " + actualInt + " is NOT smaller then " + expected;
+				status = false;
+			}
+			break;
+
+		default:
+			break;
+		}
+
+	}
+
+	@Override
+	public Class<?> getActualClass() {
+		return Integer.class;
+	}
+
+	/**
+	 * Sets the actual object to perfrom assertion on
+	 */
+	@Override
+	public void setActual(Object actual) {
+		if (actual != null) {
+			actualInt = (Integer) actual;
+		}
+	}
+
+}

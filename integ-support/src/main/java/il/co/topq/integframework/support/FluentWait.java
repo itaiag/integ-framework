5035ae2dddb479978f315d3a0afbfb81add1efb3
aharon hacmon <aharon.topq@gmail.com>
2014-01-26 17:59:24 +0200

    toString method of the fluent wait object will now contain the name of the last Function (=expected condition)'s toString (useful with validations

--- integ-support/src/main/java/il/co/topq/integframework/support/FluentWait.java
+++ integ-support/src/main/java/il/co/topq/integframework/support/FluentWait.java
@@ -4,7 +4,11 @@ package il.co.topq.integframework.support;
 import org.openqa.selenium.support.ui.Clock;
 import org.openqa.selenium.support.ui.Sleeper;
 
+import com.google.common.base.Function;
+
 public class FluentWait<T> extends org.openqa.selenium.support.ui.FluentWait<T> {
+
+	private String lastFunctionTitle;
 
 	public FluentWait(T input, Clock clock, Sleeper sleeper) {
 		super(input, clock, sleeper);
@@ -13,6 +17,20 @@ public class FluentWait<T> extends org.openqa.selenium.support.ui.FluentWait<T> 
 	public FluentWait(T input) {
 		super(input);
 	}
+	
+	@Override
+	public <V> V until(Function<? super T, V> isTrue) {
+		lastFunctionTitle = isTrue.toString();
+		return super.until(isTrue);
+	}
+	/**
+	 * return the title of the expected condition that was applied on the input.
+	 */
+	@Override
+	public String toString() {
+		return lastFunctionTitle;
+	}
+	
 	  /**
 	   * Throws a timeout exception. This method may be overridden to throw an exception that is
 	   * idiomatic for a particular test infrastructure, such as an AssertionError in JUnit4.

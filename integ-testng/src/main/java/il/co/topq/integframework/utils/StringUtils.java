463166f2b04b5aa7e962f1848103bc30c84c0c28
aharon hacmon <aharon.topq@gmail.com>
2014-02-03 11:40:46 +0200

    +getStackTrace

--- integ-testng/src/main/java/il/co/topq/integframework/utils/StringUtils.java
+++ integ-testng/src/main/java/il/co/topq/integframework/utils/StringUtils.java
@@ -1,11 +1,26 @@
 package il.co.topq.integframework.utils;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 public abstract class StringUtils {
 	private final boolean __;
-	private StringUtils(){
-		__=true;
+
+	private StringUtils() {
+		__ = true;
 	}
+
 	public static boolean isEmpty(String str) {
-		return (str==null)?true:str.isEmpty();
+		return (str == null) ? true : str.isEmpty();
+	}
+
+	public static String getStackTrace(Throwable t) {
+		if (t != null) {
+			StringWriter stringWriter = new StringWriter();
+			PrintWriter printWriter = new PrintWriter(stringWriter);
+			t.printStackTrace(printWriter);
+			return stringWriter.toString();
+		}
+		return "";
 	}
 }

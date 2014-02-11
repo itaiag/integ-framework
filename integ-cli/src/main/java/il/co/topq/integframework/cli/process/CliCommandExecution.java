4b4e1129310464dc2a81a84a32c07e69de56ff4a
aharon hacmon <aharon.topq@gmail.com>
2014-02-11 11:13:58 +0200

    1. support errors 2. getting the real result (experimental)

--- integ-cli/src/main/java/il/co/topq/integframework/cli/process/CliCommandExecution.java
+++ integ-cli/src/main/java/il/co/topq/integframework/cli/process/CliCommandExecution.java
@@ -1,22 +1,25 @@
 package il.co.topq.integframework.cli.process;
 
+import il.co.topq.integframework.AbstractModule;
+import il.co.topq.integframework.assertion.IAssertionLogic;
+import il.co.topq.integframework.cli.conn.CliCommand;
+import il.co.topq.integframework.cli.conn.CliConnection;
+import il.co.topq.integframework.utils.StringUtils;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-import il.co.topq.integframework.AbstractModule;
-import il.co.topq.integframework.cli.conn.CliCommand;
-import il.co.topq.integframework.cli.conn.CliConnection;
-import il.co.topq.integframework.utils.StringUtils;
 
 public class CliCommandExecution {
 	private final CliConnection cliConnection;
 	private long timeout = -1;
 	private String title = "";
 	private String cmd = "";
-	private List<String> musts;
+	private List<String> musts, errors;
 	protected String result;
+	protected final List<IAssertionLogic<String>> assrtions;
+
 	public CliCommandExecution(CliConnection cliConnection) {
 		this(cliConnection, "");
 	}
@@ -24,6 +27,7 @@ public class CliCommandExecution {
 	public CliCommandExecution(CliConnection cliConnection, String command) {
 		this.cliConnection = cliConnection;
 		this.cmd = command;
+		assrtions = new ArrayList<IAssertionLogic<String>>();
 	}
 
 	public CliCommandExecution withTimeout(long timeout) {
@@ -58,14 +62,29 @@ public class CliCommandExecution {
 		if (StringUtils.isEmpty(cmd)) {
 			throw new NullPointerException("command is not set");
 		}
+
 		CliCommand cliCommand = new CliCommand(cmd);
-		if (musts!=null && !musts.isEmpty()) {
+		if (musts != null && !musts.isEmpty()) {
 			cliCommand.addMusts(musts);
+		}
+		if (errors != null && !errors.isEmpty()) {
+			for (String error : errors) {
+				cliCommand.addErrors(error);
+			}
 		}
 		cliCommand.setTimeout(timeout);
 		this.cliConnection.handleCliCommand(title, cliCommand);
-		if (cliConnection instanceof AbstractModule){
-			setResult(((AbstractModule)cliConnection).getActual(String.class));
+
+		if (cliConnection instanceof AbstractModule) {
+			String result = ((AbstractModule) cliConnection).getActual(String.class);
+			String commandLine = cmd + this.cliConnection.getEnterStr();
+
+			result = StringUtils.getFirstSubStringSuffix(result, commandLine);
+
+			result = StringUtils.getPrefix(result, this.cliConnection.getEnterStr());
+
+
+			setResult(result.trim());
 		}
 	}
 
@@ -75,5 +94,33 @@ public class CliCommandExecution {
 
 	protected void setResult(String result) {
 		this.result = result;
+	}
+
+	// public String untilResponse(final AbstractAssertionLogic<String> logic) {
+	// CliConnectionWait wait = new CliConnectionWait(cliConnection);
+	//
+	// return wait.until(new CliExpectedCondition<String>() {
+	// @Override
+	// public String apply(CliConnection input) {
+	// try {
+	// execute();
+	// Assert.assertLogic(getResult(), logic);
+	// return getResult();
+	//
+	// } catch (Exception e) {
+	// throw new RuntimeException(e);
+	// }
+	// }
+	// });
+	//
+	//
+	// }
+
+	public CliCommandExecution error(String... errors) {
+		if (this.errors == null || this.errors.isEmpty()) {
+			this.errors = new ArrayList<String>(errors.length);
+		}
+		this.errors.addAll(Arrays.asList(errors));
+		return this;
 	}
 }

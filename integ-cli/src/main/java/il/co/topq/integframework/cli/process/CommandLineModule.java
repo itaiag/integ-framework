eeecb3aecfb10a3cb8b8017019da49e15656548b
aharon hacmon <aharon.topq@gmail.com>
2014-02-17 10:44:51 +0200

    new: Command line module that supports command line execution.

---
+++ integ-cli/src/main/java/il/co/topq/integframework/cli/process/CommandLineModule.java
@@ -0,0 +1,22 @@
+package il.co.topq.integframework.cli.process;
+
+import il.co.topq.integframework.AbstractModuleImpl;
+import il.co.topq.integframework.cli.conn.CliConnectionImpl;
+
+public class CommandLineModule extends AbstractModuleImpl {
+
+	protected CliConnectionImpl cliConnectionImpl;
+
+	public CliCommandExecution execute() {
+		return new CliCommandExecution(cliConnectionImpl);
+	}
+
+	public CliConnectionImpl getCliConnectionImpl() {
+		return cliConnectionImpl;
+	}
+
+	public void setCliConnectionImpl(CliConnectionImpl cliConnectionImpl) {
+		this.cliConnectionImpl = cliConnectionImpl;
+	}
+
+}

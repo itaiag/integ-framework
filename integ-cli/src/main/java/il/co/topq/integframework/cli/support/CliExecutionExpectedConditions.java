ea99fb1520f4202375bf7443cf3d4f845403874d
aharon hacmon <aharon.topq@gmail.com>
2014-02-24 18:19:12 +0200

    Fixed conditions of CliExecution

--- integ-cli/src/main/java/il/co/topq/integframework/cli/support/CliExecutionExpectedConditions.java
+++ integ-cli/src/main/java/il/co/topq/integframework/cli/support/CliExecutionExpectedConditions.java
@@ -2,6 +2,8 @@ package il.co.topq.integframework.cli.support;
 
 import il.co.topq.integframework.assertion.AbstractAssertionLogic;
 import il.co.topq.integframework.assertion.Assert;
+import il.co.topq.integframework.assertion.FailSafeAssertionListener;
+import il.co.topq.integframework.assertion.FindTextAssertion;
 import il.co.topq.integframework.cli.process.CliCommandExecution;
 
 public abstract class CliExecutionExpectedConditions {
@@ -16,12 +18,17 @@ public abstract class CliExecutionExpectedConditions {
 			@Override
 			public String apply(CliCommandExecution execution) {
 				try {
+					FailSafeAssertionListener<String> failSafeListener = new FailSafeAssertionListener<String>();
 					execution.execute();
+					Assert.assertLogic(execution.getResult(), logic, failSafeListener);
+					if (failSafeListener.getSuppressedThrowables().hasNext()) {
+						Assert.fail("execution failed", failSafeListener.getSuppressedThrowables().next());
+					}
+					return execution.getResult();
 				} catch (Exception e) {
 					Assert.fail(toString(), e);
 				}
-				Assert.assertLogic(execution.getResult(), logic);
-				return execution.getResult();
+				return null;
 			}
 
 			@Override
@@ -32,25 +39,53 @@ public abstract class CliExecutionExpectedConditions {
 
 	}
 
-	public static CliExecutionExpectedCondition<String> executionResponseReturn(final String mustHaveResponse) {
-		return new CliExecutionExpectedCondition<String>() {
+	public static CliExecutionExpectedCondition<String> executionResponseReturn(final String expectedResponse) {
+		return executionLogicHappens(new FindTextAssertion(expectedResponse) {
 			@Override
-			public String apply(CliCommandExecution execution) {
-				try {
-					execution.mustHaveResponse(mustHaveResponse).execute();
-				} catch (Exception e) {
-					Assert.fail(toString(), e);
-				}
-				return execution.getResult();
+			public String toString() {
+				return "execution response must contain " + expectedResponse;
+			}
+		});
+		// final FindTextAssertion findTextAssertion = new
+		// FindTextAssertion(mustHaveResponse);
+		// final FailSafeAssertionListener<String> failSafeListener = new
+		// FailSafeAssertionListener<String>();
+		// return new CliExecutionExpectedCondition<String>() {
+		// @Override
+		// public String apply(CliCommandExecution execution) {
+		// String result;
+		// try {
+		// execution.execute();
+		// Assert.assertLogic(result = execution.getResult(), findTextAssertion,
+		// failSafeListener);
+		// return result;
+		// } catch (Exception e) {
+		// Assert.fail(toString(), e);
+		// }
+		// return null;
+		// }
+		//
+		// @Override
+		// public String toString() {
+		// return "execution must have response with " + mustHaveResponse;
+		// }
+		// };
+
+	}
+
+	public static CliExecutionExpectedCondition<String> executionResponseReturnExactly(final String expectedResponse) {
+		return executionLogicHappens(new AbstractAssertionLogic<String>() {
+
+			@Override
+			public void doAssertion() {
+				this.status = this.actual.equals(expectedResponse);
 			}
 
 			@Override
 			public String toString() {
-				return "execution must have response of " + mustHaveResponse;
+				return "execution response must be " + expectedResponse;
 			}
-		};
-
+		});
 	}
-
 
 }

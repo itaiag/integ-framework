073173e286bcf72a322f615dee3b3d5699690fb1
aharon hacmon <aharon.topq@gmail.com>
2014-02-26 14:35:22 +0200

    Parser.parse throws ParseException

--- integ-cli/src/main/java/il/co/topq/integframework/cli/support/Parser.java
+++ integ-cli/src/main/java/il/co/topq/integframework/cli/support/Parser.java
@@ -1,5 +1,7 @@
 package il.co.topq.integframework.cli.support;
 
+import java.text.ParseException;
+
 public interface Parser<T> {
-	T parse(String source);
+	T parse(String source) throws ParseException;
 }

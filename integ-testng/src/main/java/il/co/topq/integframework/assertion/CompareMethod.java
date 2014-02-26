39c8c60cdf5c7b94b8815e766292c541530f27af
aharon hacmon <aharon.topq@gmail.com>
2014-02-26 14:34:48 +0200

    toString returns more meaningful result

--- integ-testng/src/main/java/il/co/topq/integframework/assertion/CompareMethod.java
+++ integ-testng/src/main/java/il/co/topq/integframework/assertion/CompareMethod.java
@@ -1,6 +1,22 @@
 package il.co.topq.integframework.assertion;
 
 public enum CompareMethod {
-	BIGGER, BIGGER_OR_EQUALS, EQUALS, SMALLER_OR_EQUALS, SMALLER
+	BIGGER, BIGGER_OR_EQUALS, EQUALS, SMALLER_OR_EQUALS, SMALLER;
+	public String toString() {
+		switch (this) {
+		case BIGGER:
+			return "bigger then";
+		case BIGGER_OR_EQUALS:
+			return "bigger then or equals to";
+		case EQUALS:
+			return "equals to";
+		case SMALLER_OR_EQUALS:
+			return "smaller then or equals to";
+		case SMALLER:
+			return "smaller then";
+		default:
+			return null;
+		}
 
+	};
 }

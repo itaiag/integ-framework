b7b04a22441c0864ed64fa741e3ee81fbfa59927
aharon hacmon <aharon.topq@gmail.com>
2014-03-03 16:05:28 +0200

    javadocs

--- integ-testng/src/main/java/il/co/topq/integframework/assertion/AssertionListener.java
+++ integ-testng/src/main/java/il/co/topq/integframework/assertion/AssertionListener.java
@@ -1,7 +1,23 @@
 package il.co.topq.integframework.assertion;
 
+/**
+ * 
+ * Listen to assertion pseodo-events:
+ * <ol>
+ * <li>pass</li>
+ * <li>fail</li>
+ * <li>exception thrown</li>
+ * </ol>
+ * 
+ * @author Aharon Hacmon
+ * 
+ * @param <T>
+ *            the type of actual. you may get it's value
+ */
 public interface AssertionListener<T> {
 	public void assertionPassed(final T actual, final AbstractAssertionLogic<T> logic);
+
 	public void assertionFailed(final T actual, final AbstractAssertionLogic<T> logic);
+
 	public void assertionFailed(final T actual, final AbstractAssertionLogic<T> logic, Throwable t);
 }

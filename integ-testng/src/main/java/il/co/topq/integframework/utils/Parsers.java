05d9867292dcbbdca172e2dd6cad642a4db66381
aharon hacmon <aharon.topq@gmail.com>
2014-03-17 11:50:42 +0200

    Parses for int and long

---
+++ integ-testng/src/main/java/il/co/topq/integframework/utils/Parsers.java
@@ -0,0 +1,28 @@
+package il.co.topq.integframework.utils;
+
+import java.text.ParseException;
+
+public class Parsers {
+
+	private final Object __;
+
+	private Parsers() {
+		__ = null;
+	}
+
+	public static final Parser<Long> longParser = new Parser<Long>() {
+		@Override
+		public Long parse(String source) throws ParseException {
+			return Long.parseLong(source);
+		}
+	};
+
+	public static final Parser<Integer> intParser = new Parser<Integer>() {
+		@Override
+		public Integer parse(String source) throws ParseException {
+			return Integer.parseInt(source);
+		}
+	};
+
+
+}

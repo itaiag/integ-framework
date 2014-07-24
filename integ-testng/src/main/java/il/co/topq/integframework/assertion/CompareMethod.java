abaf679848a54a554ddd5f660e6728d2ed41802f
aharon hacmon <aharon.topq@gmail.com>
2014-07-24 11:46:11 +0300

    Before and after fluent code with "IS"

--- integ-testng/src/main/java/il/co/topq/integframework/assertion/CompareMethod.java
+++ integ-testng/src/main/java/il/co/topq/integframework/assertion/CompareMethod.java
@@ -10,10 +10,22 @@ public enum CompareMethod {
 			return o1.compareTo(o2) > 0;
 		}
 	},
+	AFTER {
+		@Override
+		public <T extends Comparable<T>> boolean compare(T o1, T o2) {
+			return BIGGER.compare(o1, o2);
+		}
+	},
 	BIGGER_OR_EQUALS {
 		@Override
 		public <T extends Comparable<T>> boolean compare(T o1, T o2) {
 			return o1.compareTo(o2) >= 0;
+		}
+	},
+	AFTER_OR_WHEN {
+		@Override
+		public <T extends Comparable<T>> boolean compare(T o1, T o2) {
+			return BIGGER_OR_EQUALS.compare(o1, o2);
 		}
 	},
 	EQUALS {
@@ -22,10 +34,23 @@ public enum CompareMethod {
 			return o1.compareTo(o2) == 0;
 		}
 	},
+	WHEN {
+		@Override
+		public <T extends Comparable<T>> boolean compare(T o1, T o2) {
+			return EQUALS.compare(o1, o2);
+		}
+
+	},
 	SMALLER_OR_EQUALS {
 		@Override
 		public <T extends Comparable<T>> boolean compare(T o1, T o2) {
 			return o1.compareTo(o2) <= 0;
+		}
+	},
+	BEFORE_OR_WHEN {
+		@Override
+		public <T extends Comparable<T>> boolean compare(T o1, T o2) {
+			return SMALLER_OR_EQUALS.compare(o1, o2);
 		}
 	},
 	SMALLER {
@@ -33,19 +58,36 @@ public enum CompareMethod {
 		public <T extends Comparable<T>> boolean compare(T o1, T o2) {
 			return o1.compareTo(o2) < 0;
 		}
+	},
+	BEFORE {
+		@Override
+		public <T extends Comparable<T>> boolean compare(T o1, T o2) {
+			return SMALLER.compare(o1, o2);
+		}
+
 	};
 	public String toString() {
 		switch (this) {
 		case BIGGER:
 			return "bigger then";
+		case AFTER:
+			return "after";
 		case BIGGER_OR_EQUALS:
 			return "bigger then or equals to";
+		case AFTER_OR_WHEN:
+			return "after or when";
 		case EQUALS:
 			return "equals to";
+		case WHEN:
+			return "when";
 		case SMALLER_OR_EQUALS:
 			return "smaller then or equals to";
+		case BEFORE_OR_WHEN:
+			return "before or when";
 		case SMALLER:
 			return "smaller then";
+		case BEFORE:
+			return "before";
 		default:
 			return null;
 		}
@@ -66,5 +108,9 @@ public enum CompareMethod {
 				return me.compare(t, that);
 			}
 		};
+	}
+
+	public static final <T extends Comparable<T>> boolean is(T one, CompareMethod compare, T another) {
+		return compare.compare(one, another);
 	}
 }

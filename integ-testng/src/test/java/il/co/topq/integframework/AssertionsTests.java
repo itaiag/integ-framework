c0dbf554444b6f10e16ce997b105d6d9a90a6d06
aharon hacmon <aharon.topq@gmail.com>
2014-10-20 18:07:54 +0300

    cleanup code new tests

--- integ-testng/src/test/java/il/co/topq/integframework/AssertionsTests.java
+++ integ-testng/src/test/java/il/co/topq/integframework/AssertionsTests.java
@@ -1,46 +1,77 @@
 package il.co.topq.integframework;
 
-import il.co.topq.integframework.assertion.Assert;
+import static il.co.topq.integframework.assertion.Assert.assertLogic;
+import static java.lang.String.CASE_INSENSITIVE_ORDER;
+import static org.testng.Assert.assertNull;
+import static org.testng.collections.Lists.newArrayList;
 import il.co.topq.integframework.assertion.CollectionAssertion;
 import il.co.topq.integframework.assertion.FailSafeAssertionListener;
 
 import java.util.List;
 
 import org.testng.annotations.Test;
-import org.testng.collections.Lists;
 
 public class AssertionsTests {
 
 	@Test
 	public void testAssertCollection() {
-		Assert.assertLogic(Lists.newArrayList("A", "S", "D", "F"),
-				new CollectionAssertion<>(Lists.newArrayList("F", "D", "A", "S")));
+		assertLogic(newArrayList("A", "S", "D", "F"), new CollectionAssertion<>(newArrayList("F", "D", "A", "S")));
 	}
 
 	@Test(expectedExceptions = AssertionError.class, expectedExceptionsMessageRegExp = "Total items not found: \\d+")
 	public void testAssertCollectionFails() throws Throwable {
 		FailSafeAssertionListener<List<String>> assertionListener = new FailSafeAssertionListener<>();
-		List<String> actual = Lists.newArrayList("A", "S", "D", "F"), expected = Lists.newArrayList("Q", "W", "E", "A");
-		CollectionAssertion<String> assertion = new CollectionAssertion<>(expected);
-		Assert.assertLogic(actual, assertion, assertionListener);
+		assertLogic(newArrayList("A", "S", "D", "F"), new CollectionAssertion<>(newArrayList("Q", "W", "E", "A")),
+				assertionListener);
 		throw assertionListener.getRootSuppressedThrowable();
 	}
 
 	@Test(expectedExceptions = AssertionError.class, expectedExceptionsMessageRegExp = "Duplicates found in actual")
 	public void testAssertCollectionActualDuplications() throws Throwable {
 		FailSafeAssertionListener<List<String>> assertionListener = new FailSafeAssertionListener<>();
-		List<String> actual = Lists.newArrayList("A", "A", "D", "F"), expected = Lists.newArrayList("Q", "W", "E", "R");
-		CollectionAssertion<String> assertion = new CollectionAssertion<>(expected);
-		Assert.assertLogic(actual, assertion, assertionListener);
+		assertLogic(newArrayList("A", "A", "D", "F"), new CollectionAssertion<>(newArrayList("Q", "W", "E", "R")),
+				assertionListener);
 		throw assertionListener.getRootSuppressedThrowable();
 	}
 
 	@Test(expectedExceptions = AssertionError.class, expectedExceptionsMessageRegExp = "Duplicates found in expected")
 	public void testAssertCollectionExpectedDuplications() throws Throwable {
 		FailSafeAssertionListener<List<String>> assertionListener = new FailSafeAssertionListener<>();
-		List<String> actual = Lists.newArrayList("A", "S", "D", "F"), expected = Lists.newArrayList("Q", "W", "R", "R");
-		CollectionAssertion<String> assertion = new CollectionAssertion<>(expected);
-		Assert.assertLogic(actual, assertion, assertionListener);
+		assertLogic(newArrayList("A", "S", "D", "F"), new CollectionAssertion<>(newArrayList("Q", "W", "R", "R")),
+				assertionListener);
 		throw assertionListener.getRootSuppressedThrowable();
 	}
+
+	@Test(expectedExceptions = AssertionError.class, expectedExceptionsMessageRegExp = "all items should exists and nothing more")
+	public void testAssertCollectionMoreActuals() throws Throwable {
+		FailSafeAssertionListener<List<String>> assertionListener = new FailSafeAssertionListener<>();
+		assertLogic(newArrayList("A", "S", "D", "F", "Q"),
+				new CollectionAssertion<>(newArrayList("A", "S", "D", "F")).containsAllItems(), assertionListener);
+		throw assertionListener.getRootSuppressedThrowable();
+	}
+
+	@Test
+	public void testAssertCollectionMoreActualsOK() throws Throwable {
+		FailSafeAssertionListener<List<String>> assertionListener = new FailSafeAssertionListener<>();
+		assertLogic(newArrayList("A", "S", "D", "F", "Q"), new CollectionAssertion<>(newArrayList("A", "S", "D", "F")),
+				assertionListener);
+		assertNull(assertionListener.getRootSuppressedThrowable());
+	}
+
+	@Test
+	public void testAssertCollectionComparator() throws Throwable {
+		FailSafeAssertionListener<List<String>> assertionListener = new FailSafeAssertionListener<>();
+		assertLogic(newArrayList("A", "S", "D", "F"),
+				new CollectionAssertion<>(newArrayList("a", "s", "D", "F")).compareWith(CASE_INSENSITIVE_ORDER), assertionListener);
+		assertNull(assertionListener.getRootSuppressedThrowable());
+	}
+
+	@Test(expectedExceptions = AssertionError.class, expectedExceptionsMessageRegExp = "Duplicates found in actual")
+	public void testAssertCollectionComparatorDuplicates() throws Throwable {
+		FailSafeAssertionListener<List<String>> assertionListener = new FailSafeAssertionListener<>();
+		assertLogic(newArrayList("A", "S", "D", "F", "a"),
+				new CollectionAssertion<>(newArrayList("a", "s", "D", "F")).compareWith(CASE_INSENSITIVE_ORDER), assertionListener);
+		throw assertionListener.getRootSuppressedThrowable();
+	}
+
 }

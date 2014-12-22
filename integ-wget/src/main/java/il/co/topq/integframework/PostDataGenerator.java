fbac42f0268389adbdc32805cb10282ad33ce908
aharon hacmon <aharon.topq@gmail.com>
2014-12-22 11:30:35 +0200

    Post data generator is now cleaner

--- integ-wget/src/main/java/il/co/topq/integframework/PostDataGenerator.java
+++ integ-wget/src/main/java/il/co/topq/integframework/PostDataGenerator.java
@@ -1,5 +1,17 @@
 package il.co.topq.integframework;
 
+import com.google.common.base.Supplier;
+
 public interface PostDataGenerator {
-	public CharSequence generateData(WgetClient forClient);
+
+	public void finish();
+
+	public boolean isDone();
+
+	public void post(String data);
+
+	public String getPostData() throws InterruptedException;
+
+	public int getCapacity();
+
 }
---
+++ integ-wget/src/main/java/il/co/topq/integframework/PostDataGeneratorImpl.java
@@ -0,0 +1,50 @@
+package il.co.topq.integframework;
+
+import java.util.Queue;
+import java.util.concurrent.*;
+
+public class PostDataGeneratorImpl implements PostDataGenerator {
+
+	private boolean finish = false;
+	private final ArrayBlockingQueue<String> queue;
+	private final int capacity;
+
+	public PostDataGeneratorImpl(int clientCapacity) {
+		queue = new ArrayBlockingQueue<>(capacity = clientCapacity);
+	}
+
+	@Override
+	public void finish() {
+		this.finish = true;
+	}
+
+	@Override
+	public boolean isDone() {
+		return this.finish && this.queue.isEmpty();
+	}
+
+	@Override
+	public void post(String data) {
+		queue.add(data);
+	}
+
+	@Override
+	public int getCapacity() {
+		return Math.max(capacity, queue.size());
+	}
+
+	@Override
+	public String getPostData() throws InterruptedException {
+		String res = null;
+		while (res == null) {
+			res = queue.poll(1, TimeUnit.SECONDS);
+			
+			if (res ==null && this.isDone()) {
+				throw new InterruptedException("done");
+			}
+		}
+		return res;
+
+	}
+
+}

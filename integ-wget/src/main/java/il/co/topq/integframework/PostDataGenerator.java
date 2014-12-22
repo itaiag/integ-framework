dce1311ce291a3af6c02e7f915a34e462888e528
aharon hacmon <aharon.topq@gmail.com>
2014-12-22 12:06:37 +0200

    +javadoc

--- integ-wget/src/main/java/il/co/topq/integframework/PostDataGenerator.java
+++ integ-wget/src/main/java/il/co/topq/integframework/PostDataGenerator.java
@@ -1,17 +1,39 @@
 package il.co.topq.integframework;
 
-import com.google.common.base.Supplier;
-
 public interface PostDataGenerator {
-
+	/**
+	 * notify this {@link PostDataGenerator} that no more data will be sent
+	 */
 	public void finish();
 
+	/**
+	 * 
+	 * @return true if no more data available and the generator is finished its
+	 *         work
+	 */
 	public boolean isDone();
 
+	/**
+	 * send data later
+	 * 
+	 * @param data
+	 */
 	public void post(String data);
 
+	/**
+	 * client should get data from this method and send it. this method may
+	 * block until new data exists
+	 * 
+	 * @return data or null if there is no more data
+	 * @throws InterruptedException
+	 *             when this thread is interrupted while waiting for data
+	 */
 	public String getPostData() throws InterruptedException;
 
+	/**
+	 * 
+	 * @return the capacity that this generator can contain.
+	 */
 	public int getCapacity();
 
 }

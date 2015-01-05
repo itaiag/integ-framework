07cc9aa4bab151009658a372532f432da3f77b12
aharon hacmon <aharon.topq@gmail.com>
2015-01-05 08:40:53 +0200

    added POST with media type

--- integ-rest/src/main/java/il/co/topq/integframework/rest/RESTClientModule.java
+++ integ-rest/src/main/java/il/co/topq/integframework/rest/RESTClientModule.java
@@ -1,6 +1,9 @@
 package il.co.topq.integframework.rest;
 
 import static com.sun.jersey.api.client.Client.create;
+
+import javax.ws.rs.core.MediaType;
+
 import il.co.topq.integframework.AbstractModuleImpl;
 import il.co.topq.integframework.reporting.Reporter;
 import il.co.topq.integframework.reporting.Reporter.Color;
@@ -36,6 +39,29 @@ public class RESTClientModule extends AbstractModuleImpl {
 
 		return clientResponse;
 	}
+	
+	public ClientResponse post(String postData,MediaType postDataMediaType, String action) throws Exception {
+		Reporter.log("Request", action + "\nBody:" + postData, Style.PLAINTEXT);
+		StringBuilder builder = new StringBuilder(uri);
+		if (!uri.endsWith("/")) {
+			builder.append("/");
+		}
+		builder.append(action);
+		ClientResponse clientResponse = create().resource(builder.toString()).type(postDataMediaType)
+				.accept("application/json").post(ClientResponse.class, postData);
+
+		setActual(clientResponse.getEntity(String.class));
+		try {
+			JSONObject response = new JSONObject(getActual(String.class));
+			Reporter.log("Response", response.toString(3), Style.PLAINTEXT);
+		} catch (JSONException e) {
+			Reporter.log("Response", getActual(String.class), Style.PLAINTEXT);
+			// should never happen!!
+		}
+
+		return clientResponse;
+	}
+	
 
 	public ClientResponse get(String action) throws Exception {
 		Reporter.log("Request", action, Style.PLAINTEXT);
